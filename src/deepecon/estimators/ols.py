656501218deea35cc3b9eaabbea2d5d704959c8b
SongTan <sepinetam@gmail.com>
2025-08-18 17:39:22 +0800

    Update ols.py

--- src/deepecon/estimators/ols.py
+++ src/deepecon/estimators/ols.py
@@ -14,26 +14,39 @@ import pandas as pd
 
 from ..core.base import EstimatorBase
 from ..core.condition import Condition
-from ..core.errors import VarNotFoundError
+from ._out import EstimatorResult
 
 
 class OrdinaryLeastSquares(EstimatorBase):
     name = "ols"
 
     def options(self) -> Dict[str, str]:
-        return self.std_ops(["y_col", "X_cols"])
+        return self.std_ops(
+            keys=["y_col", "X_cols"],
+            add_ops={
+                "is_cons": "Whether to add a constant column to the design matrix",
+            }
+        )
 
     def estimator(self,
                   y_col: str,
                   X_cols: List[str],
                   _if_exp: Optional[Condition] = None,
-                  is_cons: Optional[str] = None,
-                  *args, **kwargs) -> pd.DataFrame:
+                  weight: Optional[str] = None,
+                  is_cons: bool = True,
+                  *args, **kwargs) -> EstimatorResult:
         # make sure all the args are exist and prepare data
         target_columns: List[str] = [y_col] + X_cols
         self.pre_process(target_columns, _if_exp)
         y_data = self.df[y_col].to_numpy(dtype=float)
         X_data = self.df[X_cols].to_numpy(dtype=float)
+        if is_cons:
+            X_data = np.hstack([np.ones((X_data.shape[0], 1)), X_data])
+        # n is the number of samples, k is the number of independent variables
+        n, k = X_data.shape
 
-        # main algorithm
+        beta_hat = np.linalg.inv(X_data.T @ X_data) @ X_data.T @ y_data
+        y_hat = X_data @ beta_hat
+        residuals = y_data - y_hat
+
         pass
